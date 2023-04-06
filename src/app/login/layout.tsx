9574191e49b2f5989f0d8e4080cf36e36b2f4de2
Lynxgsm <aili.fida.ac@gmail.com>
2023-04-06 09:10:07 +0300

    added server auth

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,5 @@
+const LoginLayout = () => {
+  return <div>Enter</div>;
+};
+
+export default LoginLayout;
---
+++ src/app/login/page.tsx
@@ -0,0 +1,5 @@
+const LoginPage = () => {
+  return <div>Enter</div>;
+};
+
+export default LoginPage;
